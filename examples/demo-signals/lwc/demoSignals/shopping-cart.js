7f46c8bff347107d6c1a42cb2823b1e54b924b44
cesarParra <cesar.parra@gmail.com>
2024-05-29 19:35:25 -0400

    Shopping cart can now mutate the cart

--- examples/demo-signals/lwc/demoSignals/shopping-cart.js
+++ examples/demo-signals/lwc/demoSignals/shopping-cart.js
@@ -1,4 +1,7 @@
-import {$resource} from 'c/signals';
-import getShoppingCart from '@salesforce/apex/ShoppingCartController.getShoppingCart';
+import { $resource } from "c/signals";
+import getShoppingCart from "@salesforce/apex/ShoppingCartController.getShoppingCart";
 
-export const {data: shoppingCart} = $resource(getShoppingCart);
+export const { data: shoppingCart, mutate: updateCart } = $resource(
+  getShoppingCart,
+  {}
+);
--- examples/shopping-cart/lwc/shoppingCartDetails/shoppingCartDetails.js
+++ examples/shopping-cart/lwc/shoppingCartDetails/shoppingCartDetails.js
@@ -1,6 +1,6 @@
 import TwElement from "c/twElement";
 import { $computed } from "c/signals";
-import { shoppingCart } from "c/demoSignals";
+import { shoppingCart, updateCart } from "c/demoSignals";
 
 // States
 import ready from "./states/ready.html";
@@ -11,7 +11,11 @@ export default class ShoppingCartDetails extends TwElement {
   itemData = $computed(() => (this.itemData = shoppingCart.value)).value;
 
   render() {
-    return this.itemData.loading ? loading : this.items.length > 0 ? ready : empty;
+    return this.itemData.loading
+      ? loading
+      : this.items.length > 0
+        ? ready
+        : empty;
   }
 
   get quantityOptions() {
@@ -20,20 +24,23 @@ export default class ShoppingCartDetails extends TwElement {
       { label: "2", value: 2 },
       { label: "3", value: 3 },
       { label: "4", value: 4 },
-      { label: "5", value: 5 },
+      { label: "5", value: 5 }
     ];
   }
 
   get items() {
     return this.itemData.data.items.map((item) => {
-      return {...item, total: item.price * item.quantity};
+      return { ...item, total: item.price * item.quantity };
     });
   }
 
   removeItem(event) {
     event.preventDefault();
     const itemId = event.target.dataset.item;
-    // TODO: How to set resource data
+    updateCart({
+      ...this.itemData,
+      items: this.items.filter((item) => item.id !== itemId)
+    });
     //SharedState.setData({...this.itemData, items: this.items.filter((item) => item.id !== itemId)});
   }
 
@@ -42,11 +49,10 @@ export default class ShoppingCartDetails extends TwElement {
     const newQuantity = event.target.value;
     const newItems = this.items.map((item) => {
       if (item.id === itemId) {
-        return {...item, quantity: newQuantity};
+        return { ...item, quantity: newQuantity };
       }
       return item;
     });
-    // TODO
-    //SharedState.setData({...this.itemData, items: newItems});
+    updateCart({ ...this.itemData, items: newItems });
   }
 }
