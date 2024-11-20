769b5220193982e9f274c207b1a728c23cf4ed1b
cesarParra <cesar.parra@gmail.com>
2024-11-20 09:07:56 -0400

    Introducing the observable membrane code

---
+++ src/lwc/signals/observable-membrane/observable-membrane.ts
@@ -0,0 +1,137 @@
+/* eslint-disable */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+/*
+ * Copyright (c) 2023, Salesforce.com, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import {
+    unwrap,
+    isArray,
+    isUndefined,
+    getPrototypeOf,
+    isFunction,
+    registerProxy,
+    ProxyPropertyKey,
+    ObjectDotPrototype,
+} from './shared';
+import { ReactiveProxyHandler } from './reactive-handler';
+import { ReadOnlyHandler } from './read-only-handler';
+import { init as initDevFormatter } from './reactive-dev-formatter';
+
+/* istanbul ignore else */
+if (process.env.NODE_ENV !== 'production') {
+    initDevFormatter();
+}
+
+type ValueObservedCallback = (obj: any, key: ProxyPropertyKey) => void;
+type ValueMutatedCallback = (obj: any, key: ProxyPropertyKey) => void;
+type IsObservableCallback = (value: any) => boolean;
+
+export interface ObservableMembraneInit {
+    valueMutated?: ValueMutatedCallback;
+    valueObserved?: ValueObservedCallback;
+    valueIsObservable?: IsObservableCallback;
+    tagPropertyKey?: ProxyPropertyKey;
+}
+
+function defaultValueIsObservable(value: any): boolean {
+    // intentionally checking for null
+    if (value === null) {
+        return false;
+    }
+
+    // treat all non-object types, including undefined, as non-observable values
+    if (typeof value !== 'object') {
+        return false;
+    }
+
+    if (isArray(value)) {
+        return true;
+    }
+
+    const proto = getPrototypeOf(value);
+    return proto === ObjectDotPrototype || proto === null || getPrototypeOf(proto) === null;
+}
+
+const defaultValueObserved: ValueObservedCallback = (obj: any, key: ProxyPropertyKey) => {
+    /* do nothing */
+};
+const defaultValueMutated: ValueMutatedCallback = (obj: any, key: ProxyPropertyKey) => {
+    /* do nothing */
+};
+
+function createShadowTarget(value: any): any {
+    return isArray(value) ? [] : {};
+}
+
+export class ObservableMembrane {
+    valueMutated: ValueMutatedCallback;
+    valueObserved: ValueObservedCallback;
+    valueIsObservable: IsObservableCallback;
+    tagPropertyKey: ProxyPropertyKey | undefined;
+
+    private readOnlyObjectGraph: WeakMap<any, any> = new WeakMap();
+    private reactiveObjectGraph: WeakMap<any, any> = new WeakMap();
+
+    constructor(options: ObservableMembraneInit = {}) {
+        const { valueMutated, valueObserved, valueIsObservable, tagPropertyKey } = options;
+        this.valueMutated = isFunction(valueMutated) ? valueMutated : defaultValueMutated;
+        this.valueObserved = isFunction(valueObserved) ? valueObserved : defaultValueObserved;
+        this.valueIsObservable = isFunction(valueIsObservable)
+            ? valueIsObservable
+            : defaultValueIsObservable;
+        this.tagPropertyKey = tagPropertyKey;
+    }
+
+    getProxy(value: any) {
+        const unwrappedValue = unwrap(value);
+        if (this.valueIsObservable(unwrappedValue)) {
+            // When trying to extract the writable version of a readonly we return the readonly.
+            if (this.readOnlyObjectGraph.get(unwrappedValue) === value) {
+                return value;
+            }
+            return this.getReactiveHandler(unwrappedValue);
+        }
+        return unwrappedValue;
+    }
+
+    getReadOnlyProxy(value: any) {
+        value = unwrap(value);
+        if (this.valueIsObservable(value)) {
+            return this.getReadOnlyHandler(value);
+        }
+        return value;
+    }
+
+    unwrapProxy(p: any) {
+        return unwrap(p);
+    }
+
+    private getReactiveHandler(value: any): any {
+        let proxy = this.reactiveObjectGraph.get(value);
+        if (isUndefined(proxy)) {
+            // caching the proxy after the first time it is accessed
+            const handler = new ReactiveProxyHandler(this, value);
+            proxy = new Proxy(createShadowTarget(value), handler);
+            registerProxy(proxy, value);
+            this.reactiveObjectGraph.set(value, proxy);
+        }
+        return proxy;
+    }
+
+    private getReadOnlyHandler(value: any): any {
+        let proxy = this.readOnlyObjectGraph.get(value);
+        if (isUndefined(proxy)) {
+            // caching the proxy after the first time it is accessed
+            const handler = new ReadOnlyHandler(this, value);
+            proxy = new Proxy(createShadowTarget(value), handler);
+            registerProxy(proxy, value);
+            this.readOnlyObjectGraph.set(value, proxy);
+        }
+        return proxy;
+    }
+}
---
+++ src/lwc/signals/observable-membrane/shared.ts
@@ -0,0 +1,73 @@
+/* eslint-disable */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+/*
+ * Copyright (c) 2023, Salesforce.com, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+const { isArray } = Array;
+
+const {
+    prototype: ObjectDotPrototype,
+    getPrototypeOf,
+    create: ObjectCreate,
+    defineProperty: ObjectDefineProperty,
+    isExtensible,
+    getOwnPropertyDescriptor,
+    getOwnPropertyNames,
+    getOwnPropertySymbols,
+    preventExtensions,
+    hasOwnProperty,
+} = Object;
+
+const { push: ArrayPush, concat: ArrayConcat } = Array.prototype;
+
+export {
+    ArrayPush,
+    ArrayConcat,
+    isArray,
+    getPrototypeOf,
+    ObjectCreate,
+    ObjectDefineProperty,
+    ObjectDotPrototype,
+    isExtensible,
+    getOwnPropertyDescriptor,
+    getOwnPropertyNames,
+    getOwnPropertySymbols,
+    preventExtensions,
+    hasOwnProperty,
+};
+
+const OtS = {}.toString;
+export function toString(obj: any): string {
+    if (obj && obj.toString) {
+        return obj.toString();
+    } else if (typeof obj === 'object') {
+        return OtS.call(obj);
+    } else {
+        return obj + '';
+    }
+}
+
+export function isUndefined(obj: any): obj is undefined {
+    return obj === undefined;
+}
+
+export function isFunction(obj: any): obj is Function {
+    return typeof obj === 'function';
+}
+
+const proxyToValueMap: WeakMap<object, any> = new WeakMap();
+
+export function registerProxy(proxy: object, value: any) {
+    proxyToValueMap.set(proxy, value);
+}
+
+export const unwrap = (replicaOrAny: any): any => proxyToValueMap.get(replicaOrAny) || replicaOrAny;
+
+// In the specification for Proxy, the keys are defined not as PropertyKeys (i.e. `string | symbol | number`)
+// but as `string | symbol`. See: https://github.com/microsoft/TypeScript/pull/35594
+export type ProxyPropertyKey = string | symbol;
