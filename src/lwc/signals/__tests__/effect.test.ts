33c46ea9a546dd5843f0ea8c50dc15b41cdb25ee
cesarParra <cesar.parra@gmail.com>
2024-12-03 07:22:17 -0400

    Additional unit tests

--- src/lwc/signals/__tests__/effect.test.ts
+++ src/lwc/signals/__tests__/effect.test.ts
@@ -1,18 +1,33 @@
 import { $signal, $effect } from "../core";
 
 describe("effects", () => {
+  test("react to the callback immediately", () => {
+    const signal = $signal(0);
+    const spy = jest.fn(() => signal.value);
+    $effect(spy);
+    expect(spy).toHaveBeenCalled();
+  });
+
   test("react to updates in a signal", () => {
     const signal = $signal(0);
-    let effectTracker = 0;
-
-    $effect(() => {
-      effectTracker = signal.value;
-    });
-
-    expect(effectTracker).toBe(0);
+    const spy = jest.fn(() => signal.value);
+    $effect(spy);
+    spy.mockReset();
 
     signal.value = 1;
-    expect(effectTracker).toBe(1);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test("react to updates in multiple signals", () => {
+    const a = $signal(0);
+    const b = $signal(0);
+    const spy = jest.fn(() => a.value + b.value);
+    $effect(spy);
+    spy.mockReset();
+
+    a.value = 1;
+    b.value = 1;
+    expect(spy).toHaveBeenCalledTimes(2);
   });
 
   test("throw an error when a circular dependency is detected", () => {
