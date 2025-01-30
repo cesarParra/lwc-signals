fc843024cb27f75a7a1a9c1df633a9b4396023b1
Cesar Parra <cesar.parra@gmail.com>
2025-01-30 11:59:24 -0400

    Bind example update (#41)

--- examples/counter/lwc/countTracker/countTracker.js
+++ examples/counter/lwc/countTracker/countTracker.js
@@ -1,9 +1,9 @@
 import { LightningElement } from "lwc";
-import { $binded, $computed } from "c/signals";
+import { $bind, $computed } from "c/signals";
 import { counter, counterPlusTwo } from "c/demoSignals";
 
 export default class CountTracker extends LightningElement {
-  bindTest = $binded(this, "bindTest").to(counter);
+  bindTest = $bind(this, "bindTest").to(counter);
 
   reactiveProperty = $computed(() => (this.reactiveProperty = counter.value))
     .value;
